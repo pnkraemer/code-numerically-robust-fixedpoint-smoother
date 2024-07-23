75d9483e2281cc5d9db5e39939552cfe1063193a
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-23 15:00:26 +0200

    Rename the directory to 'fpx': fixed-point smoothing in JAX

--- tests/test_fpx.py
+++ tests/test_fpx.py
@@ -4,18 +4,18 @@ import pytest_cases
 import jax.numpy as jnp
 import jax
 
-from fixedpointsmoother import statespace
+from fpx import fpx
 
 
 def case_ssm_conventional():
-    return statespace.ssm_conventional()
+    return fpx.ssm_conventional()
 
 
 @pytest_cases.parametrize_with_cases("ssm", cases=".")
 def test_trajectory_estimated(ssm):
     ts = jnp.linspace(0, 1)
 
-    init, model = statespace.model_car_tracking_velocity(
+    init, model = fpx.model_car_tracking_velocity(
         ts, noise=1e-2, diffusion=1.0, ssm=ssm
     )
 
@@ -24,11 +24,11 @@ def test_trajectory_estimated(ssm):
     key, subkey = jax.random.split(key, num=2)
     x0 = ssm.sample(subkey, init)
 
-    _, (latent, observations) = statespace.sample(key, x0, model, ssm=ssm)
+    _, (latent, observations) = fpx.sample(key, x0, model, ssm=ssm)
     assert latent.shape == (len(ts) - 1, 4)
     assert observations.shape == (len(ts) - 1, 2)
 
-    _, (mean, _cov) = statespace.filter_kalman(observations, init, model, ssm=ssm)
+    _, (mean, _cov) = fpx.filter_kalman(observations, init, model, ssm=ssm)
     print(jax.tree_util.tree_map(jnp.shape, (mean, _cov)))
     print(mean - latent)
 
