e7f6d1011e9d51a3dd7fce644bdaee92790e17d7
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-23 14:35:25 +0200

    Sample the car tracking model

--- src/fixedpointsmoother/ssm.py
+++ src/fixedpointsmoother/ssm.py
@@ -7,45 +7,85 @@ from typing import Callable
 
 
 @dataclasses.dataclass
-class Parametrisation:
+class SSM:
     init_rv: Callable
+    sample: Callable
+    conditional: Callable
 
 
 def param_conventional():
-    return Parametrisation(init_rv=lambda *a: a)
+    def sample_(key, rv):
+        mean, cov = rv
+        base = jax.random.normal(key, shape=mean.shape, dtype=mean.dtype)
+        return mean + jnp.linalg.cholesky(cov) @ base
+
+    def conditional(x, /, model):
+        A, (mean, cov) = model
+        return A @ x + mean, cov
+
+    return SSM(init_rv=lambda *a: a, sample=sample_, conditional=conditional)
 
 
 def param_square_root():
     def init_rv(m, C):
         return m, jnp.linalg.cholesky(C)
 
-    return Parametrisation(init_rv=init_rv)
+    def sample_(key, rv):
+        mean, cholesky = rv
+        base = jax.random.normal(key, shape=mean.shape, dtype=mean.dtype)
+        return mean + cholesky @ base
+
+    def conditional_(x, /, model):
+        A, (mean, cholesky) = model
+        return (A @ x + mean, cholesky)
+
+    return SSM(init_rv=init_rv, sample=sample_, conditional=conditional_)
 
 
 def model_car_tracking_velocity(ts, /, noise, diffusion, *, param):
     def transition(dt):
         eye_d = jnp.eye(2)
+        one_d = jnp.ones((2,))
 
         A_1d = jnp.asarray([[1.0, dt], [0, 1.0]])
         q_1d = jnp.asarray([0.0, 0.0])
-        Q_1d = diffusion * jnp.asarray([[dt**3 / 3, dt**2 / 2], [dt**2 / 2, dt]])
+        Q_1d = diffusion**2 * jnp.asarray([[dt**3 / 3, dt**2 / 2], [dt**2 / 2, dt]])
         H_1d = jnp.asarray([1.0, 0.0])
         r_1d = jnp.asarray(0.0)
-        R_1d = noise * jnp.asarray(1.0)
+        R_1d = noise**2 * jnp.asarray(1.0)
 
         A = jnp.kron(A_1d, eye_d)
-        q = jnp.kron(q_1d, eye_d)
+        q = jnp.kron(q_1d, one_d)
         Q = jnp.kron(Q_1d, eye_d)
         H = jnp.kron(H_1d, eye_d)
-        r = jnp.kron(r_1d, eye_d)
+        r = jnp.kron(r_1d, one_d)
         R = jnp.kron(R_1d, eye_d)
 
         rv_q = param.init_rv(q, Q)
         rv_r = param.init_rv(r, R)
         return (A, rv_q), (H, rv_r)
 
-    return jax.vmap(transition)(jnp.diff(ts))
+    m0 = jnp.zeros((4,))
+    C0 = jnp.eye(4)
+    x0 = param.init_rv(m0, C0)
+
+    return x0, jax.vmap(transition)(jnp.diff(ts))
 
 
-def sample(key, model, *, param):
-    pass
+def sample(key, init, model, *, param):
+    def scan_fun(x, model_k):
+        key_k, sample_k = x
+        model_prior, model_obs = model_k
+
+        key_k, subkey_k = jax.random.split(key_k, num=2)
+        rv = param.conditional(sample_k, model_prior)
+        sample_k = param.sample(subkey_k, rv)
+
+        key_k, subkey_k = jax.random.split(key_k, num=2)
+        rv_obs = param.conditional(sample_k, model_obs)
+        sample_obs_k = param.sample(subkey_k, rv_obs)
+        return (key_k, sample_k), (sample_k, sample_obs_k)
+
+    key, subkey = jax.random.split(key, num=2)
+    x0 = param.sample(subkey, init)
+    return jax.lax.scan(scan_fun, xs=model, init=(key, x0))
--- tests/test_kalman.py
+++ tests/test_kalman.py
@@ -5,24 +5,29 @@ import pytest_cases
 import jax.numpy as jnp
 import jax
 
+import matplotlib.pyplot as plt
+
 
 def case_param_conventional():
     return ssm.param_conventional()
 
 
-def case_param_square_root():
-    return ssm.param_square_root()
+# def case_param_square_root():
+#     return ssm.param_square_root()
 
 
 @pytest_cases.parametrize_with_cases("param", cases=".")
 def test_trajectory_estimated(param):
-    ts = jnp.linspace(0, 1, 7)
+    ts = jnp.linspace(0, 1)
 
-    model = ssm.model_car_tracking_velocity(ts, noise=0.1, diffusion=1.0, param=param)
+    init, model = ssm.model_car_tracking_velocity(
+        ts, noise=1e-2, diffusion=1.0, param=param
+    )
+
     key = jax.random.PRNGKey(seed=1)
-    data = ssm.sample(key, model, param=param)
 
-    print(model)
-    print(data)
+    _, (_, observations) = ssm.sample(key, init, model, param=param)
+    plt.plot(observations[:, 0], observations[:, 1], "o-")
+    plt.show()
 
     assert False
