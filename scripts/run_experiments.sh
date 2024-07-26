cbc0cd7b051f41e4a8727c68697f1d521bbd1fce
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-26 09:47:25 +0200

    List the running variable in the progressbar

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,1 +1,14 @@
+echo ""
+echo "Running wall-time experiment"
+echo ""
 time python experiments/measure_wall_time.py
+
+echo ""
+echo "Running memory experiment"
+echo ""
+time python experiments/measure_memory.py
+
+echo ""
+echo "Running stability experiment"
+echo ""
+time python experiments/measure_stability.py
