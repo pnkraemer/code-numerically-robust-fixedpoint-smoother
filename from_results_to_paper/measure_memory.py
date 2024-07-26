c3980ef9353db873544d72e87136efffe2a053e3
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-26 09:24:25 +0200

    Clean up the measure-memory code

--- from_results_to_paper/measure_memory.py
+++ from_results_to_paper/measure_memory.py
@@ -2,49 +2,28 @@ import pickle
 
 import jax.numpy as jnp
 import pandas as pd
+from fpx import eval_utils
 
-
-def format_tex(float_number):
-    # Taken from:
-    # https://stackoverflow.com/questions/41157879/python-pandas-how-to-format-big-numbers-in-powers-of-ten-in-latex
-    exponent = jnp.floor(jnp.log10(float_number))
-    mantissa = float_number / 10**exponent
-    mantissa_format = str(mantissa)[0:3]
-    return r"${0} \times 10^{{{1}}}$".format(mantissa_format, str(int(exponent)))
-
-
-def sizeof_fmt(num, suffix="B"):
-    # https://stackoverflow.com/questions/1094841/get-a-human-readable-version-of-a-file-size
-    for unit in ("", "K", "M", "G", "T", "P", "E", "Z"):
-        if abs(num) < 1024.0:
-            return f"{num:3.0f} {unit}{suffix}"
-        num /= 1024.0
-    return f"{num:.1f}Yi{suffix}"
-
-
-dirname = str(__file__)
-dirname = dirname.replace("from_results_to_paper", "results")
-dirname = dirname.replace(".py", "")
-
-filename = f"{dirname}/results.pkl"
+# Load the results
+filename = eval_utils.filename_results(__file__, replace="from_results_to_paper")
 with open(filename, "rb") as f:
     results = pickle.load(f)
 
+# Turn the dict into a data-frame
+df = pd.DataFrame(results)
 
-for label, value in results.items():
-    print(label)
-    print(value)
+# Format the numbers
+# Multiply the strings by 4 because this turns
+# the number of single-precision floats into bytes
+# (32-bit precision; 8 bits are one byte)
+df_formatted = df.map(lambda s: eval_utils.format_tex(4 * s))
 
-    df = pd.DataFrame(value)
-
-    df2 = df.map(lambda s: format_tex(4 * s))
-    # df2 = df.map(lambda s: sizeof_fmt(4 * s))
-    print()
-
-    print()
-    print(df.to_latex())
-    print()
-    print()
-    print(df2.to_latex())
-    print()
-    print()
+# Print a latex-compatible version of the frame
+print()
+print()
+print(df_formatted.to_latex())
+print()
+print()
+print(df_formatted.T.to_latex())
+print()
+print()
