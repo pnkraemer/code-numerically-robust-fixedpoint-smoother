af0a4a2a5e010513eb5c55b60076596f449b4a9d
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-26 11:55:38 +0200

    Clarify the BVP-calibration code

--- experiments/calibrate_bvp_solver.py
+++ experiments/calibrate_bvp_solver.py
@@ -8,85 +8,102 @@ from fpx import fpx
 def main():
     jax.config.update("jax_enable_x64", True)
 
+    num_iterations = 2
+
+    # Select a BVP
     t0 = 1.0 / (jnp.pi * 3)
     t1 = 1.0
-    y0 = 0.0
-    y1 = jnp.sin(1.0)
+    y0 = jnp.atleast_1d(0.0)
+    y1 = jnp.atleast_1d(jnp.sin(1.0))
 
+    def vector_field(t, *xs):
+        # Same as in matlab:
+        # https://www.mathworks.com/help/matlab/ref/bvp5c.html
+        return xs[2] + 2 * xs[1] / t + xs[0] / t**4
+
+    # Build a state-space model
+    num = 5
     impl = fpx.impl_cholesky_based()
-    ts = jnp.linspace(t0, t1, endpoint=True, num=1000)
+    ts = jnp.linspace(t0, t1, endpoint=True, num=100)
+    init = model_init(impl=impl, num=num)
+    latent = model_latent(ts, impl=impl, num=num)
+    constraint = model_constraint(ts, vf=vector_field, impl=impl, num=num)
 
-    init, prior = prior_transitions(ts, impl=impl)
-    # init_mean = init.mean
-    init = impl.rv_from_sqrtnorm(10 * jnp.ones_like(init.mean), init.cholesky)
-    constraint = information_operator(ts, impl=impl)
-    # constraint = replace_bcond_t1(y1, impl=impl, cond=constraint)
-    initconstraint = jax.tree.map(lambda s: s[0, ...], constraint)
-    constraint = jax.tree.map(lambda s: s[1:, ...], constraint)
+    # Replace the final constraint with the BCond
+    constraint = model_constraint_replace_y1(y1, cond=constraint, impl=impl)
 
-    # Update init
-    A = jnp.eye(6)[[0], ...]
-    r = jnp.zeros((1,))
-    R = jnp.zeros((1, 1))
-    cond = fpx.SSMCond(A, noise=impl.rv_from_sqrtnorm(r, R))
-    _, cond_rev = impl.bayes_update(init, cond)
-    init_new = impl.conditional_parametrize(jnp.atleast_1d(y0), cond_rev)
-    _, cond_rev = impl.bayes_update(init_new, initconstraint)
-    init_new = impl.conditional_parametrize(jnp.zeros_like(r), cond_rev)
+    # We handle the initial constraint separately, so it must be
+    # split from the remaining constraints
+    constraint_t0 = jax.tree.map(lambda s: s[0, ...], constraint)
+    constraint_ts = jax.tree.map(lambda s: s[1:, ...], constraint)
 
-    for _ in tqdm.tqdm(range(10)):
-        # Final SSM:
-        ssm = fpx.SSM(init=init_new, dynamics=fpx.SSMDynamics(prior, constraint))
-        # Run an RTS smoother and plot the solution
-        rts = fpx.compute_fixedinterval(impl)
-        data = jnp.zeros((len(ts[1:]), 1))
+    # Update the initial condition on y0
+    interp_t0 = model_interpolation(impl=impl, num=num)
+    _, cond_interp_t0 = impl.bayes_update(init, interp_t0)
+    init = impl.conditional_parametrize(y0, cond_interp_t0)
 
-        (final, conds), aux = rts(data, ssm)
+    # Update the initial condition on the ODE constraint
+    zeros = jnp.zeros((1,))
+    _, cond_constraint_t0 = impl.bayes_update(init, constraint_t0)
+    init = impl.conditional_parametrize(zeros, cond_constraint_t0)
 
-        A = jnp.eye(6)[[0], ...]
-        r = jnp.zeros((1,))
-        R = jnp.zeros((1, 1))
-        cond = fpx.SSMCond(A, noise=impl.rv_from_sqrtnorm(r, R))
-        _, cond_rev = impl.bayes_update(final, cond)
-        final = impl.conditional_parametrize(jnp.atleast_1d(y1), cond_rev)
+    # Construct the state-space model including
+    # initial condition, constraints, and latent dynamics
+    dynamics = fpx.SSMDynamics(latent, constraint_ts)
+    ssm = fpx.SSM(init=init, dynamics=dynamics)
+    data = jnp.zeros((len(ts[1:]), 1))
 
-        marginalize = fpx.compute_stats_marginalize(impl=impl, reverse=True)
-        marginals = marginalize(final, conds)
+    # Construct a fixed-point smoother
+    fp_smoother = fpx.compute_fixedpoint(impl=impl)
 
-        mean_new = marginals.mean[0, :]
-        stack = jnp.concatenate(
-            [marginals.cholesky[0].T, (mean_new - init_new.mean)[None, ...]], axis=0
-        )
-        cholesky_new = jnp.linalg.qr(stack, mode="r").T
-        init_new = impl.rv_from_sqrtnorm(mean_new, cholesky_new)
+    # Run a few fixed-point smoother iterations
+    for _ in tqdm.tqdm(range(num_iterations)):
+        init_estimated, _info = fp_smoother(data=data, ssm=ssm)
+        init = em_update_init(init, init_estimated, impl=impl)
+        print(init.mean)
 
-    # marginals = aux["filter_distributions"]
+    print("Final estimate:", init.mean)
+    # Construct a fixed-interval smoother so we can plot the solution
+    fi_smoother = fpx.compute_fixedinterval(impl=impl)
+    (final, conds), _info = fi_smoother(data=data, ssm=ssm)
+
+    marginalize = fpx.compute_stats_marginalize(impl=impl, reverse=True)
+    marginals = marginalize(final, conds)
+
+    # Plot the results
     plt.plot(ts, marginals.mean[:, 0], label="Approximation")
     plt.plot(ts, jnp.sin(1 / ts), label="Truth")
     plt.legend()
     plt.show()
 
-    error = jax.vmap(res)(ts, *(marginals.mean.T))
-    plt.semilogy(ts, jnp.abs(error), label="Approximation")
+    # Plot the residuals
+    error = jax.vmap(vector_field)(ts, *marginals.mean.T)
+    plt.semilogy(ts, jnp.abs(error), label="Residual")
+    plt.legend()
     plt.show()
 
 
-def res(t, x, dx, ddx, d3x, d4x, d5x):
-    # Same as in matlab:
-    # https://www.mathworks.com/help/matlab/ref/bvp5c.html
-    return ddx + 2 * dx / t + x / t**4
+def model_init(*, impl, num):
+    mean = jnp.ones((num + 1,))
+    cholesky = jnp.eye(num + 1)
+    return impl.rv_from_sqrtnorm(mean, cholesky)
 
 
-def information_operator(ts, *, impl) -> fpx.SSMCond:
-    x_and_dx = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+def model_latent(ts, *, impl, num) -> fpx.SSMCond:
+    ssm = fpx.ssm_wiener_integrated_interpolation(ts, impl=impl, num=num)
+    return ssm.dynamics.latent
+
+
+def model_constraint(ts, *, vf, impl, num) -> fpx.SSMCond:
+    x_and_dx = [1.0] * (num + 1)
     x_flat, unflatten = jax.flatten_util.ravel_pytree(x_and_dx)
 
-    def res_wrapped(t, xflat):
-        return res(t, *unflatten(xflat))
+    def vf_wrapped(t, xflat):
+        return vf(t, *unflatten(xflat))
 
     def linearized(t) -> fpx.SSMCond:
-        H = jax.jacfwd(res_wrapped, argnums=1)(t, x_flat)
-        h = res_wrapped(t, x_flat)[None]
+        H = jax.jacfwd(vf_wrapped, argnums=1)(t, x_flat)
+        h = vf_wrapped(t, x_flat)[None]
         h = h - H @ x_flat
         noise = impl.rv_from_sqrtnorm(h, 0 * jnp.eye(1))
         return fpx.SSMCond(H[None], noise=noise)
@@ -94,7 +111,7 @@ def information_operator(ts, *, impl) -> fpx.SSMCond:
     return jax.vmap(linearized)(ts)
 
 
-def replace_bcond_t1(y1, impl, cond: fpx.SSMCond) -> fpx.SSMCond:
+def model_constraint_replace_y1(y1, cond: fpx.SSMCond, impl) -> fpx.SSMCond:
     A = cond.A
     noise_mean = cond.noise.mean
 
@@ -111,10 +128,22 @@ def replace_bcond_t1(y1, impl, cond: fpx.SSMCond) -> fpx.SSMCond:
     return fpx.SSMCond(A, noise)
 
 
-def prior_transitions(ts, *, impl):
-    # ssm = fpx.ssm_car_tracking_acceleration(ts, impl=impl)
-    ssm = fpx.ssm_wiener_integrated_interpolation(ts, impl=impl, num=5)
-    return ssm.init, ssm.dynamics.latent
+def model_interpolation(*, impl, num):
+    A = jnp.eye(num + 1)[[0], ...]
+    r = jnp.zeros((1,))
+    R = jnp.zeros((1, 1))
+    return fpx.SSMCond(A, noise=impl.rv_from_sqrtnorm(r, R))
+
+
+def em_update_init(init, estimated, impl):
+    # Update the mean
+    mean_new = estimated.mean
+
+    # Update the Cholesky factor
+    diff = estimated.mean - init.mean
+    stack = jnp.concatenate([init.cholesky.T, diff[None, ...]], axis=0)
+    cholesky_new = jnp.linalg.qr(stack, mode="r").T
+    return impl.rv_from_sqrtnorm(mean_new, cholesky_new)
 
 
 if __name__ == "__main__":
