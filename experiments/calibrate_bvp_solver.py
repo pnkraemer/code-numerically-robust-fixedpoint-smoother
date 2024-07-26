feecb8dfd662b5b3e4f6fa766b26f9f6f14667f4
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-26 10:23:50 +0200

    Linearize the BVP constraint correctly

--- experiments/calibrate_bvp_solver.py
+++ experiments/calibrate_bvp_solver.py
@@ -1,21 +1,40 @@
 import jax.flatten_util
 import jax.numpy as jnp
+from fpx import fpx
 
 
-def res(t, x, dx, ddx):
-    return ddx + 2 * dx / t + x / t**4
+def main():
+    t0 = 1.0 / (jnp.pi * 3)
+    t1 = 1.0
+    y0 = 0.0
+    y1 = jnp.sin(1.0)
+
+    ts = jnp.linspace(t0, t1, endpoint=True, num=10)
+    impl = fpx.impl_cholesky_based()
+    constraint = information_operator(ts, impl=impl)
+    print(jax.tree.map(jnp.shape, constraint))
 
 
-x_and_dx = [1.0, 1.0, 10.0]
-x_flat, unflatten = jax.flatten_util.ravel_pytree(x_and_dx)
+def information_operator(ts, *, impl) -> fpx.SSMCond:
+    def res(t, x, dx, ddx):
+        # Same as in matlab:
+        # https://www.mathworks.com/help/matlab/ref/bvp5c.html
+        return ddx + 2 * dx / t + x / t**4
+
+    x_and_dx = [1.0, 1.0, 10.0]
+    x_flat, unflatten = jax.flatten_util.ravel_pytree(x_and_dx)
+
+    def res_wrapped(t, xflat):
+        return res(t, *unflatten(xflat))
+
+    def linearized(t) -> fpx.SSMCond:
+        H = jax.jacfwd(res_wrapped, argnums=1)(t, x_flat)
+        h = res_wrapped(t, x_flat)
+        noise = impl.rv_from_mvnorm(jnp.zeros_like(h), jnp.eye(1))
+        return fpx.SSMCond(H[None], noise=noise)
+
+    return jax.vmap(linearized)(ts)
 
 
-def res_wrapped(t, xflat):
-    return res(t, *unflatten(xflat))
-
-
-def Ht(t):
-    return jax.jacfwd(res_wrapped, argnums=1)(t, x_flat)
-
-
-print(Ht(2.0))
+if __name__ == "__main__":
+    main()
