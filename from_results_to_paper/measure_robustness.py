5e8eb7dfcb0a828c87d306b76d6e65f4cdfb5212
Nicholas Krämer <nicokraemer@gmx.net>
2024-07-29 08:53:56 +0200

    Format the BVP plot a bit more

--- from_results_to_paper/measure_robustness.py
+++ from_results_to_paper/measure_robustness.py
@@ -19,11 +19,25 @@ means = jnp.load(f"{dirname}/mean.npy")
 
 # Plot the solution
 plt.rcParams.update(eval_utils.plot_style())
-plt.subplots(figsize=(3, 2), tight_layout=True)
+plt.subplots(figsize=(2.5, 2), tight_layout=True)
 plt.plot(ts, means[:, 0])
-plt.plot(ts[0], means[0, 0], marker=".", linestyle="None", color="C0")
-plt.plot(ts[-1], means[-1, 0], marker=".", linestyle="None", color="C0")
-plt.title("BVP Test Problem", fontsize="medium")
+plt.plot(
+    ts[0],
+    means[0, 0],
+    marker=".",
+    markerfacecolor="white",
+    linestyle="None",
+    color="C0",
+)
+plt.plot(
+    ts[-1],
+    means[-1, 0],
+    marker=".",
+    markerfacecolor="white",
+    linestyle="None",
+    color="C0",
+)
+# plt.title(r"Problem $\#$15", fontsize="medium")
 plt.xlabel("Input $t$")
 plt.ylabel("Output $x$")
 
@@ -31,7 +45,7 @@ plt.ylabel("Output $x$")
 name = str(__file__)
 name = name.replace(".py", "")
 plt.savefig(f"{name}.pdf")
-
+plt.show()
 
 # Turn the dictionary into a dataframe
 df = pd.DataFrame(results)
@@ -43,8 +57,5 @@ df_formatted = df.map(lambda s: eval_utils.format_large_number_tex(s, num_digits
 print()
 print()
 print(df_formatted.to_latex())
-print()
-print()
-print(df_formatted.T.to_latex())
 print()
 print()
